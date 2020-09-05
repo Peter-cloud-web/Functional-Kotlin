a3397fccf9467d06b009acd3b49e3ceac694727a
Peter-cloud-web <petergichia165@gmail.com>
2020-09-05 23:31:08 +0300

    using anstract classes

--- src/BakeryGood.kt
+++ src/BakeryGood.kt
@@ -1,13 +1,18 @@
 //parent class
 //To extend a class in kotlin or to override a method they have to be open
-open class BakeryGood(val flavour: String) {
+//changing from open to abstract class
+//open classes can be instantiated but abstract cant
+//open class BakeryGood(val flavour: String) changes to...
+abstract class BakeryGood(val flavour: String) {
     //class must be open to be overridden from parent class to child class
     //use open if the method will altered in the subclasses
     open fun eat(): String {
         return "delicious $flavour bakery good"
     }
-    open fun name():String{
-        return "Bakery Good"
-    }
+
+    //abstract methods in kotlin are bodyless with only
+    //methods name,numbers,parameter type and return type
+    abstract fun name():String
+
 }
 
